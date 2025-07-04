0e2077481d7a37d2e887b64197ae2ffafa271c8b
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-07-04 11:47:26 +0100

    change the logo name

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
       <div className={styles.logoContainer}>
         <a href="/" className={styles.logoLink}>
           <span className={styles.logoText}>
-            {['U', 'n', 'i', 'v', 'a', 'd', 'e'].map((letter, index) => (
+            {['M', 'n', 'i', 'v', 'a', 'd', 'e'].map((letter, index) => (
               <span 
                 key={index}
                 className={styles.logoLetter}
